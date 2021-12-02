02f7ee108fd4f4bd2a5d378c46aa94c5f34382c6
fabien hollebeque <hollebeque.fabien@hotmail.com>
2021-12-02 19:10:44 +0100

    Ajout du contenu de la page d'index

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
+use App\Repository\Moderation\BanRepository;
+use App\Repository\Moderation\CommentsRepository;
+use App\Repository\Player\PlayersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    private PlayersRepository $playersRepository;
+    private CommentsRepository $commentsRepository;
+    private BanRepository $banRepository;
+
+    public function __construct(
+        PlayersRepository $playersRepository,
+        CommentsRepository $commentsRepository,
+        BanRepository $banRepository
+    ) {
+        $this->playersRepository = $playersRepository;
+        $this->commentsRepository = $commentsRepository;
+        $this->banRepository = $banRepository;
+    }
+
+
     /**
      * @Route("/", name="default", methods={"GET"})
      */
     public function index(): Response
     {
-        return $this->render('default/index.html.twig');
+        $players = $this->playersRepository->findAll();
+        $warns = $this->commentsRepository->findBy(['type' => 'WARNING']);
+        $bans = $this->banRepository->findBy(['banState' => true]);
+
+        return $this->render('default/index.html.twig', [
+            'nbPlayer' => count($players),
+            'nbWarn' => count($warns),
+            'nbBan' => count($bans),
+        ]);
     }
 }
